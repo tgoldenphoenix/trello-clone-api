4de9bf73b944a084c15257a97773c67eefa31c00
tgoldenphoenix <anhaophamx@gmail.com>
2025-06-23 08:36:27 +0700

    add API update user information

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { userValidation } from '~/validations/userValidation'
 import { userController } from '~/controllers/userController'
-// import { authMiddleware } from '~/middlewares/authMiddleware'
+import { authMiddleware } from '~/middlewares/authMiddleware'
 // import { multerUploadMiddleware } from '~/middlewares/multerUploadMiddleware'
 
 const Router = express.Router()
@@ -21,12 +21,12 @@ Router.route('/logout')
 Router.route('/refresh_token')
   .get(userController.refreshToken)
 
-// Router.route('/update')
-//   .put(
-//     authMiddleware.isAuthorized,
-//     multerUploadMiddleware.upload.single('avatar'),
-//     userValidation.update,
-//     userController.update
-//   )
+Router.route('/update')
+  .put(
+    authMiddleware.isAuthorized,
+    // multerUploadMiddleware.upload.single('avatar'),
+    userValidation.update,
+    userController.update
+  )
 
 export const userRoute = Router
